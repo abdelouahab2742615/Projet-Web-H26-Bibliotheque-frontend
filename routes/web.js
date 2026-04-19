5a85293c4ddb88645ce331cb053cd335343941ed
Rayane <hammachinerayane943@gmail.com>
2026-04-19 00:15:24 -0400

    Add Book and Category models, controllers and routes

--- routes/web.js
+++ routes/web.js
@@ -4,6 +4,10 @@ const authController = require("../controllers/authController");
 const userController = require("../controllers/userController");
 const roleController = require("../controllers/roleController");
 const authMiddleware = require("../middleware/authMiddleware");
+const categoryController = require("../controllers/categoryController");
+const bookController = require("../controllers/bookController");
+
+
 
 router.get("/", (req, res) => {
   res.redirect("/login");
@@ -29,5 +33,21 @@ router.post("/roles/create", authMiddleware, roleController.create);
 router.get("/roles/edit/:id", authMiddleware, roleController.showEdit);
 router.post("/roles/edit/:id", authMiddleware, roleController.update);
 router.post("/roles/delete/:id", authMiddleware, roleController.delete);
+
+
+// Categories
+router.get("/categories", authMiddleware, categoryController.index);
+router.get("/categories/create", authMiddleware, categoryController.showCreate);
+router.post("/categories/create", authMiddleware, categoryController.create);
+router.get("/categories/edit/:id", authMiddleware, categoryController.showEdit);
+router.post("/categories/edit/:id", authMiddleware, categoryController.update);
+router.post("/categories/delete/:id", authMiddleware, categoryController.delete);
+
+// Books
+router.get("/books", authMiddleware, bookController.index);
+router.get("/books/create", authMiddleware, bookController.showCreate);
+router.post("/books/create", authMiddleware, bookController.create);
+router.post("/books/delete/:id", authMiddleware, bookController.delete);
+
 
 module.exports = router;
